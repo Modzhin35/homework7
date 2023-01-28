793e7c2412cb84413090e9cf5fd78f13238b1ae1
modzhin <modzhds@gmail.com>
2023-01-28 18:38:45 +0300

    Task47 correct

--- task47/Program.cs
+++ task47/Program.cs
@@ -4,22 +4,27 @@
 // 1 -3,3 8 -9,9
 // 8 7,8 -7,1 9
 
-int Intput (string message){
+int Intput(string message)
+{
     Console.Write(message);
     return Convert.ToInt32(Console.ReadLine());
 }
 
-double [,] GetRandomArray (int rows, int cols, int min, int max){
-    double [,] randomArray = new double [rows, cols];
-    for(int i=0; i<rows; i++){
-        for (int j=0; j<cols; j++)randomArray[i, j]=Math.Round(new Random().Next(min, max)+new Random().NextDouble(), 1);
+double[,] GetRandomArray(int rows, int cols, int min, int max)
+{
+    double[,] randomArray = new double[rows, cols];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++) randomArray[i, j] = Math.Round(new Random().Next(min, max) + new Random().NextDouble(), 1);
     }
     return randomArray;
 }
 
-void PrintRandomArray (double [,] randArray){
-    for(int i=0; i<randArray.GetLength(0); i++){
-        for(int j=0; j<randArray.GetLength(1); j++) Console.Write(randArray[i, j]+"|");
+void PrintRandomArray(double[,] randArray)
+{
+    for (int i = 0; i < randArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < randArray.GetLength(1); j++) Console.Write(randArray[i, j] + "|");
         Console.WriteLine();
     }
 }
